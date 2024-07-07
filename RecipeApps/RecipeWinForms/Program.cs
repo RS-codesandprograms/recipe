17c174d4d38b55ef84bec6875c606da2adf2df9b
RS-codesandprograms <codesandprograms@gmail.com>
2024-07-07 16:15:05 -0500

    Security: removed connection string

--- RecipeApps/RecipeWinForms/Program.cs
+++ RecipeApps/RecipeWinForms/Program.cs
@@ -18,8 +18,7 @@ namespace RecipeWinForms
 
 
             SQLUtility.ConnectionString =
-                "Server=tcp:dev-codesandprograms.database.windows.net,1433;Initial Catalog=HeartyHearthDB;Persist Security Info = False;User ID=CodesandProgramsAdmin;Password=Hashem Yachol!!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-
+               
 
             Application.Run(new frmSearch());
         }
