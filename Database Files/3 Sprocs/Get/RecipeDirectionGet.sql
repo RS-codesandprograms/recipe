04d7c01d439bd0065680b8318643c55e581672ae
RS-codesandprograms <codesandprograms@gmail.com>
2025-07-15 22:39:39 -0500

    RecipeDirectionGet SPROC

---
+++ Database Files/3 Sprocs/Get/RecipeDirectionGet.sql
@@ -0,0 +1,26 @@
+create or alter procedure dbo.RecipeDirectionGet(
+	@RecipeDirectionId int = 0,
+	@RecipeId int = 0,
+	@Message varchar(500) = ''  output
+)
+as
+begin
+	declare @return int = 0
+
+	select  @RecipeDirectionId = isnull(@RecipeDirectionId,0), @RecipeId = isnull(@RecipeId,0)
+	
+	
+	select ri.Instruction, ri.DirectionSequence
+	from RecipeDirection ri 
+	where ri.RecipeID = @RecipeId
+	order by ri.DirectionSequence
+
+	return @return
+end
+
+go
+
+
+exec RecipeDirectionGet
+@RecipeId = 107
+--@All = 1, @IncludeBlank = 1
